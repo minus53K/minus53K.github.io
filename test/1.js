c5074a12e383177a88de9f862955172f20d95c9c
Minus53K <87570653+minus53K@users.noreply.github.com>
2023-10-15 13:32:01 +0800

    Delete test directory

--- test/1.js
+++
@@ -1,8 +0,0 @@
-var confirm = (document.getElementById("cb").onclick);
-function add() {
-    if (confirm == 1);
-    var n1 = document.getElementById("n1");
-    var n2 = document.getElementById("n2");
-    alert(n1+n2);
-}
-add();
