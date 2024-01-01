214f6931d82855fad0b5db6a89285dcff6024eec
Minus53K <87570653+minus53K@users.noreply.github.com>
2024-01-01 11:12:47 +0800

    Add files via upload

---
+++ js/head.js
@@ -0,0 +1,6 @@
+document.writeln("        <meta charset=\'utf-8\'>");
+document.writeln("        <link rel=\'icon\' href=\'img/favicon.ico\'>");
+document.writeln("        <link rel=\'stylesheet\' href=\'style/style.css\'>");
+document.writeln("        <link rel=\'stylesheet\' href=\'style/textstyle.css\'>");
+document.writeln("        <link rel=\'stylesheet\' href=\'style/nav.css\'>");
+document.writeln("        <link rel=\'stylesheet\' href=\'style/media.css\'>");
---
+++ js/header.js
@@ -0,0 +1,11 @@
+document.writeln("        <header>");
+document.writeln("            <nav>");
+document.writeln("                <ul>");
+document.writeln("                    <li><a href=\'index.html\'>首页</a></li>");
+document.writeln("                    <li><a href=\'media.html\'>媒体</a></li>");
+document.writeln("                    <li><a href=\'tools.html\'>工具</a></li>");
+document.writeln("                    <li><a href=\'nav.html\'>导航</a></li>");
+document.writeln("                    <li><a href=\'https://github.com/minus53K/minus53K.github.io\'>关于</a></li>");
+document.writeln("                </ul>");
+document.writeln("            </nav>");
+document.writeln("        </header>");
