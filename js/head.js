00749cd94b0b9b737faa9dc130793c8645b3e74f
Minus53K <87570653+minus53K@users.noreply.github.com>
2024-01-01 11:18:31 +0800

    Add files via upload

--- js/head.js
+++ js/head.js
@@ -1,4 +1,3 @@
-document.writeln("        <meta charset=\'utf-8\'>");
 document.writeln("        <link rel=\'icon\' href=\'img/favicon.ico\'>");
 document.writeln("        <link rel=\'stylesheet\' href=\'style/style.css\'>");
 document.writeln("        <link rel=\'stylesheet\' href=\'style/textstyle.css\'>");
